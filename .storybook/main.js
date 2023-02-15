19135550ad853f4d2c5c99c28a42c96f5c8ab6df
714k <victor.zamudio@hotmail.com>
2023-02-14 22:56:47 -0600

    Adding storybook

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../projects/angular-components/**/*.stories.mdx",
+    "../projects/angular-components/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/angular",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
---
+++ projects/angular-components/src/components/header-navigation/header-navigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vz-header-navigation',
+  templateUrl: './header-navigation.component.html',
+  styleUrls: ['./header-navigation.component.scss']
+})
+export class HeaderNavigationComponent {
+  title: string = "In this section";
+  anchors: object = [];
+}
